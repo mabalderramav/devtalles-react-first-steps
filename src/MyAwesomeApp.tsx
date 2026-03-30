1fc8527e6ef7da20716bc63efbc1de12829f4dbb
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-03-30 12:38:23 -0400

    adds variables to component

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,24 @@
+const name = 'Miguel Aldo';
+const lastname = 'Balderrama Vaca';
+const favoritesGames = ['League of Legends', 'Valorant', 'Overwatch'];
+const isActive = true;
+const address = {
+    city: 'Buenos Aires',
+    country: 'Argentina',
+}
 export function MyAwesomeApp() {
     return (
         <>
-            <h1>Miguel Aldo</h1>
-            <h3>Balderrama Vaca</h3>
+            <h1>{name}</h1>
+            <h3>{lastname}</h3>
+            <ul>
+                {favoritesGames.map((game) => (
+                    <li key={game}>{game}</li>
+                ))}
+            </ul>
+            {isActive && <p>I'm active</p>}
+            <h2>Address</h2>
+            <p>I live in {address.city}, {address.country}</p>
         </>
     )
 }
