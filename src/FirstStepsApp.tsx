a46146891fb1a1d06a6ec14d6686c1aaedd3d28b
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-04-02 17:39:16 -0400

    adds props, useStates and css for this components

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,12 +1,27 @@
 import {ItemCounter} from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+    productName: string;
+    quantity: number;
+}
+
+const ItemsInCart: ItemInCart[] = [
+    {productName: 'Nintendo switch 2', quantity: 1},
+    {productName: 'Word ward cratf', quantity: 2},
+    {productName: 'FIFA 2026', quantity: 3},
+    {productName: 'Super Mario 4', quantity: 5},
+]
+
 export function FirstStepsApp() {
     return (
         <>
-            <h1>Carrito de Compras</h1>
-            <ItemCounter/>
-            <ItemCounter/>
-            <ItemCounter/>
+            <h1>Shopping Cart</h1>
+            {ItemsInCart.map(({productName, quantity}) => (
+                <ItemCounter
+                    key={productName}
+                    name={productName}
+                    quantity={quantity}/>
+            ))}
         </>
     )
 }
--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,17 +1,34 @@
-export const ItemCounter = () => {
+import {useState} from "react";
+import './ItemCounter.css';
+
+interface Props {
+    name: string,
+    quantity?: number
+}
+
+export const ItemCounter = ({name, quantity = 1}: Props) => {
+    const [count, setCount] = useState(quantity);
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+    const handleSubtract = () => {
+        if (count === 1) return;
+
+        setCount(count - 1);
+    }
+
     return (
-        <section style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '10px',
-            marginBottom: '20px',
-        }}>
-            <span style={{
-                width: '160px',
-            }}> Nintendo switch 2</span>
-            <button>+1</button>
-            <span>10</span>
-            <button>-1</button>
+        <section className="item-row">
+            <span
+                className="item-text"
+                style={{
+                    color: count === 1 ? 'red' : 'green',
+                }}
+            > {name} </span>
+            <button onClick={handleAdd}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtract}>-1</button>
         </section>
     )
 }
