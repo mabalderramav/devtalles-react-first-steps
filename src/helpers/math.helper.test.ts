4a00919030bc945452b769489c24244f8de384b4
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-04-07 13:08:33 -0400

    adds tests for math.helper.ts

---
+++ src/helpers/math.helper.test.ts
@@ -0,0 +1,65 @@
+import {describe, expect, test} from "vitest";
+import {Add, Multiply, Subtract} from "./math.helper.ts";
+
+describe("Add", () => {
+    test("Should Add two positive number", () => {
+        // Arrange
+        const a = 2;
+        const b = 3;
+        // Act
+        const result = Add(a, b);
+        // Assertion
+        expect(result).toBe(a + b);
+    });
+    test("Should Add two negative number", () => {
+        // Arrange
+        const a = -2;
+        const b = -3;
+        // Act
+        const result = Add(a, b);
+        // Assert
+        expect(result).toBe(a + b);
+    });
+});
+
+describe("Subtract", () => {
+    test("Should Subtract two positive number", () => {
+        // Arrange
+        const a = 2;
+        const b = 3;
+        // Act
+        const result = Subtract(a, b);
+        // Assert
+        expect(result).toBe(a - b);
+    });
+    test("Should Subtract two negative number", () => {
+        // Arrange
+        const a = -2;
+        const b = -3;
+        // Act
+        const result = Subtract(a, b);
+        // Assert
+        expect(result).toBe(a - b);
+    });
+});
+
+describe("Multiply", () => {
+    test("Should Multiply two positive number", () => {
+        // Arrange
+        const a = 2.5;
+        const b = 3;
+        // Act
+        const result = Multiply(a, b);
+        // Assert
+        expect(result).toBe(a * b);
+    });
+    test("Should Multiply two negative number", () => {
+        // Arrange
+        const a = -2;
+        const b = -3;
+        // Act
+        const result = Multiply(a, b);
+        // Assert
+        expect(result).toBe(a * b);
+    })
+});
---
+++ src/helpers/math.helper.ts
@@ -0,0 +1,4 @@
+export const Add = (a: number, b: number): number => a + b;
+export const Subtract = (a: number, b: number): number => a - b;
+export const Multiply = (a: number, b: number): number => a * b;
+export const Divide = (a: number, b: number): number => a / b;
